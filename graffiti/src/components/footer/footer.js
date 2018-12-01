77804382a5e0f2349de65d5efe476e50e0d50def
Anthony <ajssalemo@gmail.com>
2018-12-01 16:49:34 -0500

    finished 1/3 footer link section

--- graffiti/src/components/footer/footer.js
+++ graffiti/src/components/footer/footer.js
@@ -3,11 +3,41 @@
 
 import React from 'react';
 
+// Images
+import bootstrap from '../../images/bootstrap-solid.svg';
+
 // ---------------------------------------------------------------------- //
 
 const Footer = () => {
     return (
-        <div className="footer"></div>
+        <div className="footer">
+            <div className="container pb-5">
+                <div className="row text-center">
+                    <div className="col-md-4 mt-4">
+                        <h3>Technologies</h3>
+                        <a href="https://reactjs.org/" className="mt-2 mb-2">
+                            <span className="footer-text pr-1">React.js</span><i className="fab fa-react fa-2x"></i>
+                        </a>
+                        <a href="https://nodejs.org/en/" className="mt-2 mb-2">
+                            <span className="footer-text pr-1">Node.js</span><i className="fab fa-node fa-2x"></i>
+                        </a>
+                        <a href="https://sass-lang.com/" className="mt-2 mb-2">
+                            <span className="footer-text pr-1">SASS</span><i className="fab fa-sass fa-2x"></i>
+                        </a>
+                        <a href="https://getbootstrap.com/" className="mt-2">
+                            <span className="footer-text pr-1">Bootstrap</span><img src={bootstrap} alt="Bootstrap logo" width="20" height="20"/>
+                        </a>
+                    </div>
+                    <div className="col-md-4 mt-4">
+                        <h3>Resources</h3>
+                        
+                    </div>
+                    <div className="col-md-4 mt-4">
+                        <h3>Galleries</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 };
 
