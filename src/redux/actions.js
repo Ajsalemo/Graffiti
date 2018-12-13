004af64ff23d6dd072d5f8a547be986ca4b65f98
Anthony <ajssalemo@gmail.com>
2018-12-12 19:45:17 -0500

    added search term ternary variable

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -9,8 +9,16 @@ import { LOAD_IMAGES_SUBMIT } from './types';
 
 export const returnMostRecent = query => {
     const API_KEY = process.env.REACT_APP_FLICKR_API_KEY;
-    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${query}&per_page=100&format=json&nojsoncallback=1`;
+    let searchTerm = query.toUpperCase();
+    const graffiti = 'graffiti';
 
+    const includesSearchTerm = searchTerm.includes(graffiti.toUpperCase());
+    let properSearchTerm = searchTerm += graffiti.toUpperCase();
+
+    const recontructedSearchTerm = !includesSearchTerm ? properSearchTerm : `${properSearchTerm.replace(/graffiti/gi, '')}${graffiti.toUpperCase()}`;
+      
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${recontructedSearchTerm}&per_page=100&format=json&nojsoncallback=1`;
+    console.log(url);
     return dispatch => axios.get(url)
         .then(result => {
             dispatch({
