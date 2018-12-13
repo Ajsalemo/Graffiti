1ca0a0edef3717c0bdd5029e16fcaa71a4f4eac8
Anthony <ajssalemo@gmail.com>
2018-12-12 20:33:35 -0500

    added handling for no search results

--- src/components/image-dashboard/image-dashboard.js
+++ src/components/image-dashboard/image-dashboard.js
@@ -2,7 +2,10 @@
 // ---------------------------------------------------------------------- //
 
 import React from 'react';
-import { connect } from "react-redux";
+import { connect } from 'react-redux';
+
+// Images
+import notfound from '../../images/notfound.jpg'
 
 // ---------------------------------------------------------------------- //
 // ---------------------------------------------------------------------- //
@@ -11,22 +14,32 @@ const ImageDashboard = props => {
     const { images } = props;
 
     return (
-        images.map((image, i) => {
-            const id = image.id;
-            const secret = image.secret;
-            const server = image.server;
-            const farm = image.farm;
-            const title = image.title;
-    
-            return (
-                <img 
-                    key={i} 
-                    src={`http://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`} 
-                    alt={title} 
-                    className="m-2 image-dashboard"
-                />
-            )
-        })
+        images.length ? 
+            images.map((image, i) => {
+                const id = image.id;
+                const secret = image.secret;
+                const server = image.server;
+                const farm = image.farm;
+                const title = image.title;
+        
+                return (
+                    <img 
+                        key={i} 
+                        src={`http://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`} 
+                        alt={title} 
+                        className="m-2 image-dashboard"
+                    />
+                )
+            })
+        :
+        <div>
+            <img  
+                src={notfound} 
+                alt='Not found' 
+                className="m-2 image-dashboard"
+            />
+            <span className="no-results m-3">It seems nothing was found. Try another search term</span>
+        </div>
     )
 };
 
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -10,13 +10,13 @@ import { LOAD_IMAGES_SUBMIT } from './types';
 export const returnMostRecent = query => {
     const API_KEY = process.env.REACT_APP_FLICKR_API_KEY;
     let searchTerm = query.toUpperCase();
-    const graffiti = 'graffiti';
+    const graffiti = ' graffiti';
 
     const includesSearchTerm = searchTerm.includes(graffiti.toUpperCase());
     let properSearchTerm = searchTerm += graffiti.toUpperCase();
 
     const recontructedSearchTerm = !includesSearchTerm ? properSearchTerm : `${properSearchTerm.replace(/graffiti/gi, '')}${graffiti.toUpperCase()}`;
-      
+    console.log(recontructedSearchTerm)
     const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_KEY}&tags=${recontructedSearchTerm}&per_page=100&format=json&nojsoncallback=1`;
     console.log(url);
     return dispatch => axios.get(url)
