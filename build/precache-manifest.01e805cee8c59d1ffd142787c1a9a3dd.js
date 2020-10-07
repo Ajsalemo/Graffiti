183325bd776a0b7afbca66abe6871f780226d31e
Anthony Salemo <ajssalemo@gmail.com>
2020-10-07 19:39:05 -0400

    updated this legacy project to actually serve the build folder and host with netlify

---
+++ build/precache-manifest.01e805cee8c59d1ffd142787c1a9a3dd.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8226ff481b7d99782e8",
+    "url": "/Graffiti/static/css/main.b6d9420c.chunk.css"
+  },
+  {
+    "revision": "c8226ff481b7d99782e8",
+    "url": "/Graffiti/static/js/main.c8226ff4.chunk.js"
+  },
+  {
+    "revision": "ed872a13a6a81372aea8",
+    "url": "/Graffiti/static/css/1.a1233066.chunk.css"
+  },
+  {
+    "revision": "ed872a13a6a81372aea8",
+    "url": "/Graffiti/static/js/1.ed872a13.chunk.js"
+  },
+  {
+    "revision": "216ab727ae44b7c8ed4b",
+    "url": "/Graffiti/static/js/runtime~main.216ab727.js"
+  },
+  {
+    "revision": "412ae3fa8836dada388b62586e83de3c",
+    "url": "/Graffiti/static/media/bootstrap-solid.412ae3fa.svg"
+  },
+  {
+    "revision": "3d98d599161a38fef793e8b64b00f0a4",
+    "url": "/Graffiti/static/media/streetartnyc.3d98d599.PNG"
+  },
+  {
+    "revision": "d65deea4757ec0ee7a10ff1b9e9e3990",
+    "url": "/Graffiti/static/media/artcrimes.d65deea4.gif"
+  },
+  {
+    "revision": "1f3a842931c9ef7326af053c2a1c8c1a",
+    "url": "/Graffiti/static/media/streetpins.1f3a8429.jpg"
+  },
+  {
+    "revision": "fad75b823adff41ae05e7c9b12c43906",
+    "url": "/Graffiti/static/media/jumbotron1.fad75b82.jpg"
+  },
+  {
+    "revision": "9c7bcf57ea7167ee8468e139b5e791be",
+    "url": "/Graffiti/static/media/jumbotron2.9c7bcf57.jpg"
+  },
+  {
+    "revision": "bfed4d5e394bdd9f1e4fe13b0f5b3f47",
+    "url": "/Graffiti/static/media/jumbotron3.bfed4d5e.jpg"
+  },
+  {
+    "revision": "0a787ba5b55d31c13be3540b3f472239",
+    "url": "/Graffiti/static/media/jumbotron4.0a787ba5.jpg"
+  },
+  {
+    "revision": "f6ffcd4293a1b3e03ee8e84f689ecdd7",
+    "url": "/Graffiti/static/media/jumbotron5.f6ffcd42.jpg"
+  },
+  {
+    "revision": "761830332dbef9ae8d51d1e49dc81113",
+    "url": "/Graffiti/static/media/parallax.76183033.jpg"
+  },
+  {
+    "revision": "208d7cf089cebb8843970b78b9e3ad58",
+    "url": "/Graffiti/static/media/notfound.208d7cf0.jpg"
+  },
+  {
+    "revision": "bcb5ecde6c82c7d483195d496536a2e5",
+    "url": "/Graffiti/index.html"
+  }
+];
