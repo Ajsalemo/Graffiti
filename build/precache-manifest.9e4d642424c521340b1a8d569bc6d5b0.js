2c4d6c1b6f0ac1c5540138231388d1932dbea0a7
Anthony Salemo <ajssalemo@gmail.com>
2020-10-07 20:04:02 -0400

    updated this legacy project to actually serve the build folder and host with netlify

---
+++ build/precache-manifest.9e4d642424c521340b1a8d569bc6d5b0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ec90466e7d4fc1ba5b6",
+    "url": "/static/css/main.b6d9420c.chunk.css"
+  },
+  {
+    "revision": "6ec90466e7d4fc1ba5b6",
+    "url": "/static/js/main.6ec90466.chunk.js"
+  },
+  {
+    "revision": "17b61e4b1944bfe1c40b",
+    "url": "/static/css/1.a1233066.chunk.css"
+  },
+  {
+    "revision": "17b61e4b1944bfe1c40b",
+    "url": "/static/js/1.17b61e4b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "412ae3fa8836dada388b62586e83de3c",
+    "url": "/static/media/bootstrap-solid.412ae3fa.svg"
+  },
+  {
+    "revision": "3d98d599161a38fef793e8b64b00f0a4",
+    "url": "/static/media/streetartnyc.3d98d599.PNG"
+  },
+  {
+    "revision": "d65deea4757ec0ee7a10ff1b9e9e3990",
+    "url": "/static/media/artcrimes.d65deea4.gif"
+  },
+  {
+    "revision": "1f3a842931c9ef7326af053c2a1c8c1a",
+    "url": "/static/media/streetpins.1f3a8429.jpg"
+  },
+  {
+    "revision": "fad75b823adff41ae05e7c9b12c43906",
+    "url": "/static/media/jumbotron1.fad75b82.jpg"
+  },
+  {
+    "revision": "9c7bcf57ea7167ee8468e139b5e791be",
+    "url": "/static/media/jumbotron2.9c7bcf57.jpg"
+  },
+  {
+    "revision": "bfed4d5e394bdd9f1e4fe13b0f5b3f47",
+    "url": "/static/media/jumbotron3.bfed4d5e.jpg"
+  },
+  {
+    "revision": "0a787ba5b55d31c13be3540b3f472239",
+    "url": "/static/media/jumbotron4.0a787ba5.jpg"
+  },
+  {
+    "revision": "f6ffcd4293a1b3e03ee8e84f689ecdd7",
+    "url": "/static/media/jumbotron5.f6ffcd42.jpg"
+  },
+  {
+    "revision": "761830332dbef9ae8d51d1e49dc81113",
+    "url": "/static/media/parallax.76183033.jpg"
+  },
+  {
+    "revision": "208d7cf089cebb8843970b78b9e3ad58",
+    "url": "/static/media/notfound.208d7cf0.jpg"
+  },
+  {
+    "revision": "93f69530ceef538bf938fa049b8a26d7",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Graffiti/precache-manifest.09c8ea61565d19bb4d9db91bde0da07d.js"
+  "/precache-manifest.9e4d642424c521340b1a8d569bc6d5b0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Graffiti/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
